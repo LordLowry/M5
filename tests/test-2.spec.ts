bc0e0eab92da580f2c17b61a72cf7b69fd409ede
LordLowry <richie.lowry@mail.com>
2024-03-21 16:35:43 +0000

    sky bet 1

--- tests/test-2.spec.ts
+++ tests/test-2.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('https://www.rte.ie/');
-  await page.getByRole('button', { name: 'Accept All Cookies' }).click();
-  await page.locator('#primary-nav-global').getByRole('link', { name: 'Sport' }).click();
-  await page.locator('#secondary-nav').getByRole('link', { name: 'Soccer' }).click();
+  await page.goto('https://m.skybet.com');
+  await page.getByRole('button', { name: 'Allow all cookies' }).click();
 });
+
+
