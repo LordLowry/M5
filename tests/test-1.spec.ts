03814ff71590ae63dcf42a38e82b88a26d087f88
LordLowry <richie.lowry@mail.com>
2024-03-21 21:53:25 +0000

    site

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -3,6 +3,6 @@ import { test, expect } from '@playwright/test';
 test('test', async ({ page }) => {
   test.setTimeout(10000);
   await page.goto('https://m.skybet.com');
-  await page.getByRole('link', { name: 'Sky Bet logo' }).click();
-  await page.getByRole('link', { name: 'Log in' }).click();
+  await page.goto('https://m.skybet.com/in-play');
+  await page.goto('https://m.skybet.com/promotions');
  });
