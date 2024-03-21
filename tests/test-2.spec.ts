4e3dc33f89b5cda025232e6ca2c955da5d65f008
LordLowry <richie.lowry@mail.com>
2024-03-21 16:39:18 +0000

    sky news

--- tests/test-2.spec.ts
+++ tests/test-2.spec.ts
@@ -1,8 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('https://m.skybet.com');
-  await page.getByRole('button', { name: 'Allow all cookies' }).click();
+  await page.goto('https://skynews.com');
 });
 
 
