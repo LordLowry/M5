dadd139edf290982171d11a2e2092cf7052c4b01
LordLowry <richie.lowry@mail.com>
2024-03-21 21:45:21 +0000

    new 4

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -4,6 +4,4 @@ test('test', async ({ page }) => {
   test.setTimeout(10000);
   await page.goto('https://m.skybet.com');
   await page.locator('#top-sports-nav').getByRole('link', { name: 'In-Play' }).click();
-  await page.getByText('My Bet Slip').click();
-  await page.getByRole('link', { name: 'Log in' }).click();
-  });
+ });
