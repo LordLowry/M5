cc412726e99ef358f2118b7f30ad961a8d4b2abd
LordLowry <richie.lowry@mail.com>
2024-03-21 20:49:18 +0000

    iphone

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,12 +36,12 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },*/
-
+/*
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
+*/
     /*{
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
@@ -52,10 +52,10 @@ export default defineConfig({
     //   name: 'Mobile Chrome',
     //   use: { ...devices['Pixel 5'] },
     // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+     {
+       name: 'Mobile Safari',
+       use: { ...devices['iPhone 12'] },
+     },
 
     /* Test against branded browsers. */
     // {
