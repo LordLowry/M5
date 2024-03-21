2c76af27f94671f9920a24719593369e520b7013
LordLowry <richie.lowry@mail.com>
2024-03-21 16:50:09 +0000

    sky 5

--- tests/test-2.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://skybet.com');
-  await page.getByRole('button', { name: 'Allow all cookies' }).click();
-});
-
-
