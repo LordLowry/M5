94abc616d72f43bd73f4c927c2181fba3f39e4bb
LordLowry <richie.lowry@mail.com>
2024-03-21 22:03:18 +0000

    dispatch

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -2,7 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   test.setTimeout(10000);
-  await page.goto('https://m.skybet.com');
-  await page.goto('https://m.skybet.com/in-play');
-  await page.goto('https://m.skybet.com/promotions');
- });
+  await page.goto('https://m.skybet.com/match-5');
+  await page.getByRole('button', { name: 'Allow all cookies' }).dispatchEvent('click');
+   });
