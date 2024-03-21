26abaee3c24cbd923c1760a4b37e67aa74aa70a3
LordLowry <richie.lowry@mail.com>
2024-03-21 21:05:01 +0000

    test 1

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -3,4 +3,6 @@ import { test, expect } from '@playwright/test';
 test('test', async ({ page }) => {
   test.setTimeout(10000);
   await page.goto('https://m.skybet.com/match-5');
+  await page.goto('https://rte.ie');
+  await page.goto('https://m.skybet.com/match-5');
 });
