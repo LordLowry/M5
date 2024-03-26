df2bc557d884f6a045a92c4642a51ea0761d3391
LordLowry <richie.lowry@mail.com>
2024-03-26 14:40:02 +0000

    try 2

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -3,5 +3,7 @@ import { test, expect } from '@playwright/test';
 test('test', async ({ page }) => {
   test.setTimeout(10000);
   await page.goto('https://m.skybet.com');
-  await page.getByRole('link', { name: 'Log in' }).click();
+  await page.getByRole('link', { name: 'Promotions' }).click();
+  await page.getByRole('link', { name: 'What\'s On' }).click();
+  
    });
